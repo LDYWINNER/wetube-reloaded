b9bc089db46e19a35ddfaf86d367b6d793c8fb14
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2023-01-24 03:00:54 +0900

    #7.0 Create Account part One

--- src/db.js
+++ src/db.js
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/wetube", { 
+mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
   //mongoose 6.x 버전 이상이면 해줄 필요 없긴 함
-  useNewUrlParser: true, 
-  useUnifiedTopology: true, 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
   useFindAndModify: false,
+  useCreateIndex: true,
 });
 
 const db = mongoose.connection;
 
 const handleOpen = () => console.log("Connected to DB");
 const handleError = (error) => console.log("DB Error", error);
-db.on("error", handleError); 
+db.on("error", handleError);
 db.once("open", handleOpen);
